0136c7f94c203de18cd8d96dfdd9dfb225a06303
Kirill Tsiutsiura <kirill.tsiutsiura7@gmail.com>
2022-12-24 19:35:49 +0200

    Первые два задания

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup();
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+const lightBox = new SimpleLightbox(".gallery a");
+
+lightBox.defaultOptions.captionDelay = 250;
